9eaa4ade3b0efbd84dca1a175ebff846c47b9c1f
gannagoodkevich <gannagoodkevich@gmail.com>
2019-11-15 12:19:53 +0300

    change rubocop

--- script.rb
+++ script.rb
@@ -1,72 +1,90 @@
-class Histogram_Generator
+# frozen_string_literal: true
 
-	def initialize
+class HistogramGenerator
+  def self.process_string(string:)
+    if !string.empty?
+      string = string.gsub(/[ ,.\r\n]/, '').split('')
+      string.each_with_object(Hash.new(0)) do |word, result|
+        result[word] += 1
+      end
+    else
+      {}
+    end
+  end
 
-	end
-	def self.processString(string:)
-		result = if !string.empty?
-
-			string.gsub( /[ ,.\r\n]/, " " ).split( " " ).each_with_object( Hash.new(0) ) {|word, result| result[word]+=1};
-
-	  else
-			{}
-		end
-	end
-
-	def self.sort_histogram(histogram)
-		сортировка = lambda do |word, occurencies| occurencies; end;
-		histogram.sort_by { |word, occurencies| сортировка.call(word, occurencies) }.to_h
-	end
-
+  def self.sort_histogram(histogram)
+    sorting = ->(_word, occurrences) do occurrences; end
+    histogram.sort_by do |word, occurrences|
+      sorting.call(word, occurrences)
+    end
+    histogram.to_h
+  end
 end
 
 module App
-	Text = 'When Mr. and Mrs. Dursley woke up on the dull, gray Tuesday our story
-starts, there was nothing about the cloudy sky outside to suggest that
-strange and mysterious things would soon be happening all over the
-country. Mr. Dursley hummed as he picked out his most boring tie for
+  HARRY_POTTER_PART = 'When Mr. and Mrs. Dursley woke up on the dull, gray
+Tuesday our story starts, there was nothing about the cloudy sky outside to
+suggest that strange and mysterious things would soon be happening all
+over the country. Mr. Dursley hummed as he picked out his most boring tie for
 work, and Mrs. Dursley gossiped away happily as she wrestled a screaming
 Dudley into his high chair.'
-	def perform
-		text = ''
-		puts("reading file")
-		puts("extracted string: #{text}")
-		histogram = Histogram_Generator.processString ({string: Text})
-		puts("generated histogram:")
-		puts(histogram)
-		puts("sorted histogram:")
-		puts(Histogram_Generator.sort_histogram(histogram))
-	end
-	module_function :perform
+  def perform
+    text = ''
+    puts('reading file')
+    puts("extracted string: #{text}")
+    histogram = HistogramGenerator.process_string(string: Text)
+    puts('generated histogram:')
+    puts(histogram)
+    puts('sorted histogram:')
+    puts(HistogramGenerator.sort_histogram(histogram))
+  end
 
-	def number_to_word(number)
-		case number
-			when 1 then
-				'one'
-			when 2 then
-				'two'
-			when 3 then
-				'three'
-			when 4 then
-				'four'
-			when 5 then
-				'five'
-			when 6 then
-				'six'
-			when 7 then
-				'seven'
-			when 8 then
-				'eight'
-			when 9 then
-				'nine'
-			else
-				'oops'
-		end
-	end
-	module_function :number_to_word
+  def number_to_word(number)
+    if number > 9
+      'oops'
+    else
+      send("print_#{number}")
+    end
+  end
+
+  def print_1
+    'one'
+  end
+
+  def print_2
+    'two'
+  end
+
+  def print_3
+    'three'
+  end
+
+  def print_4
+    'four'
+  end
+
+  def print_5
+    'five'
+  end
+
+  def print_6
+    'six'
+  end
+
+  def print_7
+    'seven'
+  end
+
+  def print_8
+    'eight'
+  end
+
+  def print_9
+    'nine'
+  end
 end
 
 puts App.perform
-for number in [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 13666 ]
-	puts App.number_to_word(number)
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 13_666].each do |number|
+  puts App.number_to_word(number)
 end
